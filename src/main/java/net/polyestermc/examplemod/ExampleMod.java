7f26ff7d9f4506a5e74de3e2e4df9e768f771eaa
Aaron <78169748+tl0x@users.noreply.github.com>
2022-03-28 15:53:05 -0400

    pog
    it works

---
+++ src/main/java/net/polyestermc/examplemod/ExampleMod.java
@@ -0,0 +1,10 @@
+package net.polyestermc.examplemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
